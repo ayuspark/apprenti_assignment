4200dd18367453cbbbf767467eb706d6cf495294
ayuspark <lola.luyaowang@gmail.com>
2017-08-16 14:27:38 -0700

    fixed linter problems

--- web_fund/jQuery/disappearing_ninja/app.js
+++ web_fund/jQuery/disappearing_ninja/app.js
@@ -1,12 +1,13 @@
 'use strict';
 $(function() {
-  console.log( "ready!" );
+  // console.log( "ready!" );
   var imgString = '';
   for(var i = 0; i < 8; i++){
     imgString += '<img src=\"https://pbs.twimg.com/profile_images/2370446440/6e2jwf7ztbr5t1yjq4c5.jpeg\" alt=\"nyan cat\">';
-  };
+  }
+
   $('div').append(imgString);
-  
+
   var $allImg = $('img');
   $('img').click(function () {
     $(this).css('border', '2px solid gold');
