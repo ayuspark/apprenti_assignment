02160ceb8b0f488c9e9307eb11c0b79b5ac90e2c
ayuspark <lola.luyaowang@gmail.com>
2017-09-02 18:54:12 -0700

    created class Bike

--- python/oop_assignment.py
+++ python/oop_assignment.py
@@ -1,10 +1,29 @@
-class Bike(price, max_speed, miles):
-    def __init__(self):
+class Bike(object):
+    def __init__(self, price, max_speed):
         self.price = price
         self.max_speed = max_speed
-        self.miles = miles
-        return self
-    def display_info():
+        self.miles = 0
+        # return self
+
+    def display_info(self):
         print(
-            'The price is %s, the max speed is %s, and the miles is %s' %[self.price, self.max_speed, self.miles]
+            'The price is %s, the max speed is %s, and the miles is %s'
+            % (self.price, self.max_speed, self.miles)
         )
+    
+    def ride(self):
+        self.miles += 10
+        print(self.miles)
+        return self
+
+    def reverse(self):
+        if self.miles >= 5:
+            self.miles -= 5
+        print('Reversing...', 5)
+        return self
+
+
+bike1 = Bike('55', '70')
+bike1.display_info()
+bike1.ride().ride().ride()
+bike1.reverse()
