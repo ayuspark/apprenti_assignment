34094d15a2ffcffbbd8b44ec2d78c60f0eed3fd7
ayuspark <lola.luyaowang@gmail.com>
2017-09-09 15:03:16 -0700

    make random number stay the same during each post request

--- flask_assignments_my_env/guess_number_game/routes.py
+++ flask_assignments_my_env/guess_number_game/routes.py
@@ -8,6 +8,7 @@ app.secret_key = 'realsecret'
 
 @app.route('/')
 def index():
+    session['comp_guess'] = random.randint(1, 100)
     if 'guess_time' not in session:
         session['guess_time'] = 0
     return render_template('index.html')
@@ -17,21 +18,23 @@ def index():
 def show_num():
     session['guess_time'] += 1
     if request.method == 'POST':
-        comp_guess = random.randint(1, 100)
         data_posted = request.form['guess_num']
         # data_posted = json.loads(request.data)['guess']
         print('client made the guess: ', data_posted)
-        print('comp guess:', comp_guess)
+        print('comp guess:', session['comp_guess'])
         if session['guess_time'] < 10:
-            if comp_guess == int(data_posted):
+            if session['comp_guess'] == int(data_posted):
                 response = 'Bingo'
-            elif comp_guess > int(data_posted):
+                session['guess_time'] = 0
+                session['comp_guess'] = random.randint(1, 100)
+            elif session['comp_guess'] > int(data_posted):
                 response = 'Too low'
             else:
                 response = 'Too high'
         else:
             response = 'Out of guesses!'
             session['guess_time'] = 0
+            session['comp_guess'] = random.randint(1, 100)
     
     print(response)
     return response
