323c5d0cf5d14b564b4f80dc7a2151f69efc24f2
ayuspark <lola.luyaowang@gmail.com>
2017-09-13 14:12:36 -0700

    finished holy cow login/register

--- flask_assignments_my_env/15_login_registration/server.py
+++ flask_assignments_my_env/15_login_registration/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash, session
 
 from wtforms import StringField, SubmitField, PasswordField, validators
 from wtforms.validators import Required
@@ -16,7 +16,7 @@ bootstrap = Bootstrap(app)
 mysql = MySQLConnector(app, 'login_registration')
 
 
-class NameForm(FlaskForm):
+class RegisterForm(FlaskForm):
         fname = StringField('First Name: ',
                             [validators.DataRequired(),
                              validators.Length(min=2, max=10),
@@ -38,25 +38,68 @@ class NameForm(FlaskForm):
         submit = SubmitField('Submit')
 
 
+class LoginForm(FlaskForm):
+        email = StringField('Email: ',
+                            [validators.DataRequired(),
+                            validators.Email('Invalid email')])
+        psw = PasswordField('Password: ', [validators.DataRequired()])
+        submit = SubmitField('Login')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-        form = NameForm()
-        print(form.fname.data)
+        form = RegisterForm()
         if form.validate_on_submit():
-                query = '''
-                        INSERT INTO users
-                        (fname, lname, email, psw)
-                        VALUES(:fname, :lname, :email, :psw)
-                        '''
+                query = "SELECT * FROM users WHERE email=:form_email"
                 data = {
-                        'fname': form.fname.data,
-                        'lname': form.lname.data,
-                        'email': form.email.data,
-                        'psw': form.psw.data,
-                }
-                mysql.query_db(query, data)
-                return redirect(url_for('index'))
+                        'form_email': form.email.data,
+                        }
+                result = mysql.query_db(query, data)
+                if result:
+                        flash('You have an account, please log in')
+                else:
+                        query = """
+                                INSERT INTO users
+                                (fname, lname, email, psw)
+                                VALUES(:fname, :lname, :email, :psw)
+                                """
+                        data = {
+                                'fname': form.fname.data,
+                                'lname': form.lname.data,
+                                'email': form.email.data,
+                                'psw': form.psw.data,
+                                }
+                        mysql.query_db(query, data)
+                        flash('Success!')
+                        return redirect(url_for('index'))
         return render_template('index.html', form=form)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+        form = LoginForm()
+        if 'email' not in session:
+                session.pop('_flashes')
+                session['email'] = ''
+        elif session['email'] != '':
+                print(session['email'])
+                flash('You are logged in! Your email is: %s' % (session['email']))
+        if form.validate_on_submit():
+                query = "SELECT id, psw, email FROM users WHERE email=:form_email"
+                data = {
+                        'form_email': form.email.data
+                        }
+                result = mysql.query_db(query, data)
+                print(result)
+                if result and result[0]['psw'] == form.psw.data:
+                        session['email'] = form.email.data
+                        print(session['email'])
+                        print('email: ', result[0]['email'])
+                        flash('Success!')
+                else:
+                        flash('Somgthing is fishy...')
+        return render_template('login.html', form=form)
+
 
 
 
