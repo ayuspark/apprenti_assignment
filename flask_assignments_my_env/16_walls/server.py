68da4dc4f5ebc542cee34617af36e39a0f4e639f
ayuspark <lola.luyaowang@gmail.com>
2017-09-14 12:37:08 -0700

    messing with form actions, commit before screw up

--- flask_assignments_my_env/16_walls/server.py
+++ flask_assignments_my_env/16_walls/server.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, redirect, request, url_for, flash, ses
 from wtforms import StringField, SubmitField, PasswordField, validators, TextAreaField
 from wtforms.validators import Required
 from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
 
 from flask_bootstrap import Bootstrap
 
@@ -13,8 +14,12 @@ import binascii
 from mysqlconnection import MySQLConnector
 from datetime import datetime, date
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+
+csrf = CSRFProtect(app)
+csrf.init_app(app)
 
 bootstrap = Bootstrap(app)
 mysql = MySQLConnector(app, 'walls_db')
@@ -68,18 +73,24 @@ class LoginForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-        msg_form = PostForm()
-        comment_form = CommentForm()
+        # msg_form = PostForm()
+        # comment_form = CommentForm()
         #to display all messages
         query = """SELECT CONCAT_WS(' ', users.fname, users.lname) AS name,
-                messages.msg, DATE_FORMAT(messages.created_at, \"%M %D %Y %T\") AS created_at
+                messages.msg, DATE_FORMAT(messages.created_at, \"%M %D %Y %T\") AS created_at, messages.id AS msg_id, users.id AS user_id
                 FROM messages
                 JOIN users ON messages.user_id = users.id
                 ORDER BY messages.created_at DESC
                 """
         results = mysql.query_db(query)
         # print('result: ', results)
+        return render_template('index.html',
+                        #        msg_form=msg_form,
+                        #        comment_form=comment_form,
+                               results=results)
 
+@app.route('/post', methods=['POST'])
+def post_msg():
         if 'email' in session:
                 if msg_form.validate_on_submit():
                         query = "INSERT INTO messages (msg, user_id, created_at, updated_at) VALUE(:form_msg, :session_user_id, NOW(), NOW())"
@@ -91,11 +102,30 @@ def index():
                         return redirect(url_for('index'))
         else:
                 flash('Please register or log in!')
-        return render_template('index.html',
+                return redirect(url_for('index'))
+        return render_template('msg_form.html',
                                msg_form=msg_form,
-                               comment_form=comment_form,
-                               results=results)
+                               comment_form=comment_form,)
+        
 
+@app.route('/delete', methods=['POST', 'GET'])
+@csrf.exempt
+def delete_msg():
+        if 'email' not in session or session['email'] == '':
+                flash('Please log in or register first!')
+        else:
+                if request.method == 'POST':
+                        if str(session['user_id']) == request.form['user_id']:
+                                msg_id_to_delete = request.form['msg_id']
+                                query = "DELETE FROM messages WHERE id=:msg_id"
+                                data = {
+                                        'msg_id': msg_id_to_delete
+                                        }
+                                mysql.query_db(query, data)
+                                flash('Post deleted!')
+                        else:
+                                flash('You can only delete your own posts!')
+                return redirect(url_for('index'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -149,7 +179,7 @@ def login():
                 session['email'] = ''
                 session['user_id'] = 0
         elif session['email'] != '':
-                print(session['email'])
+                print('session: ', session['email'])
                 flash('You are logged in! Your email is: %s' % (session['email']))
         if form.validate_on_submit():
                 query = "SELECT * FROM users WHERE email=:form_email"
