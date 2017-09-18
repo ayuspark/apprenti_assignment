3fb88774b337bb58fe6c168e1066271623bca404
ayuspark <lola.luyaowang@gmail.com>
2017-09-18 14:43:46 -0700

    finished request.session assignment

--- djangos/mysite/apps/time_display/urls.py
+++ djangos/mysite/apps/time_display/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.index, name='index'),
+        url(r'^rand_str$', views.rand_str),
     ]
--- djangos/mysite/apps/time_display/views.py
+++ djangos/mysite/apps/time_display/views.py
@@ -4,9 +4,23 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from time import gmtime, strftime
+from django.utils.crypto import get_random_string
 # Create your views here.
 def index(request):
     context = {
-        "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+        'time': strftime('%Y-%m-%d %H:%M %p', gmtime())
     }
     return render(request, 'time_display/index.html', context)
+
+
+def rand_str(request):
+    if 'str_count' not in request.session:
+        request.session['str_count'] = 0
+    else:
+        request.session['str_count'] += 1
+    context = {
+        'rand_str': get_random_string(length=14),
+        'str_count': request.session['str_count'],
+    }
+    print(request.session['str_count'])
+    return render(request, 'time_display/rand_str.html', context)
