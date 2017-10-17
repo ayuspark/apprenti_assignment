812cbb73e6b5c635d31b0d9e17b9fe66a5af5c81
ayuspark <lola.luyaowang@gmail.com>
2017-10-17 16:06:38 -0700

    COMPLETE deckOfCards

--- dotnet_c#/deckOfCards/Program.cs
+++ dotnet_c#/deckOfCards/Program.cs
@@ -8,9 +8,21 @@ namespace deckOfCards
         static void Main(string[] args)
         {
             Deck myDeck = new Deck();
-            myDeck.Cards = new List<Card>();
-            myDeck.CreateDeck();
             myDeck.ShowDeck();
+            myDeck.Deal();
+            myDeck.Deal();
+            myDeck.Deal();
+            myDeck.ShuffleCards();
+            Console.WriteLine(myDeck.Cards.Count);
+
+            Player myPlayer = new Player("lola");
+            Console.WriteLine(myPlayer.Name);
+            myPlayer.Draw(myDeck);
+            myPlayer.Draw(myDeck);
+            myPlayer.Draw(myDeck);
+            myPlayer.ShowHand();
+            Console.WriteLine("Hand: {0}", myPlayer.Hand.Count);
+            myPlayer.Discard(5);
         }
 
         class Card
@@ -35,8 +47,8 @@ namespace deckOfCards
         class Deck
         {
             public List<Card> Cards { set; get; }
-            public List<Card> CreateDeck(){
-                //List<object> deckOfCards = new List<object>();
+            public Deck(){
+                Cards = new List<Card>();
                 string[] suits = { "spade", "heart", "diamond", "clubs" };
                 Dictionary<int, string> stringValDict = new Dictionary<int, string>
                 {
@@ -56,11 +68,66 @@ namespace deckOfCards
                         }
                     }
                 }
-                return Cards;
+            }
+
+            public void ShuffleCards(){
+                Random rand = new Random();
+                for (int idx = 0; idx < Cards.Count - 1; idx++) {
+                    int randIdx = rand.Next(idx + 1, Cards.Count - 1);
+                    Card tempCard = Cards[idx];
+                    Cards[idx] = Cards[randIdx];
+                    Cards[randIdx] = tempCard;
+                }
+            }
+
+            public Card Deal(){
+                this.ShuffleCards();
+                Card dealtCard;
+                dealtCard = this.Cards[this.Cards.Count - 1];
+                this.Cards.RemoveAt(Cards.Count - 1);
+                Console.WriteLine("++++++++++++++++++++++++ I dealt you this card: ");
+                dealtCard.ShowCard();
+                return dealtCard;
             }
 
             public void ShowDeck(){
                 foreach (Card card in Cards){
+                    card.ShowCard();
+                }
+            }
+        }
+
+        class Player
+        {
+            public string Name { set; get; }
+            public List<Card> Hand { set; get; }
+            public Player(string name){
+                Name = name;
+                this.Hand = new List<Card>();
+            }
+            public Card Draw(Deck deck) {
+                Card cardToDraw = deck.Deal();
+                this.Hand.Add(cardToDraw);
+                Console.WriteLine("+++++++++++++++++++++++ I draw this card: ");
+                cardToDraw.ShowCard();
+                return cardToDraw;
+            }
+            public Card Discard(int idx) {
+                Console.WriteLine("+++++++++++++++++++++ I discarded this card: ");
+                try {
+                    Hand[idx].ShowCard();
+                    Card cardToDiscard = Hand[idx];
+                    Hand.RemoveAt(idx);
+                    return cardToDiscard; 
+                }
+                catch (System.ArgumentOutOfRangeException) {
+                    Console.WriteLine("++++++++++++++++++++ I am too stupid to know how many cards I have.");
+                    return null;
+                }
+            }
+            public void ShowHand() {
+                Console.WriteLine("****************************** this is my Hand: ");
+                foreach (Card card in this.Hand) {
                     card.ShowCard();
                 }
             }
