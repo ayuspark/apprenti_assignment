fd07e020451d990976292ec757d0e22ba27af3bc
ayuspark <lola.luyaowang@gmail.com>
2017-10-29 23:01:40 -0700

    finished RestoReview using EF, can't imagine how people would do it following the stupid platform 😤

---
+++ asp_identity_core_ef/ViewModels/RestoReviewViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace asp_identity_core_ef.ViewModels
+{
+    public class RestoReviewViewModel
+    {
+        [Required]
+        [MaxLength(256)]
+        [Display(Name = "Write a review")]
+        public string ReviewContent { get; set; }
+
+        [Required]
+        [Display(Name = "Resto name")]
+        public string RestoName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date visited")]
+        public DateTime DateVisited { get; set; }
+    }
+}
