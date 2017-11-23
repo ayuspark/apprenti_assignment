7dd960f232ae7971b0955775bf7ca88c9f7d89e2
ayuspark <lola.luyaowang@gmail.com>
2017-11-23 00:43:19 -0800

    generate random color bars 🌈

--- my-angular-quickstart/src/app/app.component.ts
+++ my-angular-quickstart/src/app/app.component.ts
@@ -5,6 +5,8 @@ import { ngModuleJitUrl } from '@angular/compiler';
 import { NgModel } from '@angular/forms/src/directives/ng_model';
 import { NgModule } from '@angular/core/src/metadata/ng_module';
 
+import { RGBcolor } from '../app/rgbColor';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,4 +15,39 @@ import { NgModule } from '@angular/core/src/metadata/ng_module';
 
 export class AppComponent {
   title = 'Ma First Arugular App!';
+  numberOfDivs = stringifyRGB();
+}
+
+function generateColor(): RGBcolor[]{
+  let colorArr: RGBcolor[] = [];
+  for (let i: number = 0; i < 20; i++) {
+    let rgb: RGBcolor;
+
+    // TODO: WHY it no work!
+    // rgb.r = Math.floor(Math.random() * (255 - 0 + 1));
+    // rgb.g = Math.floor(Math.random() * (255 - 0 + 1));
+    // rgb.b = Math.floor(Math.random() * (255 - 0 + 1));
+
+    rgb = {
+      r: Math.floor(Math.random() * (255 - 0 + 1)),
+      g: Math.floor(Math.random() * (255 - 0 + 1)),
+      b: Math.floor(Math.random() * (255 - 0 + 1)),
+    }
+
+    colorArr.push(rgb);
+  }
+  return colorArr;
+}
+
+const colorArr = generateColor();
+
+function stringifyRGB(): any[]{
+  let rgbStringArr: string[] = [];
+  for(let i = 0; i < colorArr.length; i++)
+  {
+    let rgbString : string = "";
+    rgbString = "rgb(" + colorArr[i].r + "," + colorArr[i].g + "," + colorArr[i].b + ")"; 
+    rgbStringArr.push(rgbString);
+  }
+  return rgbStringArr;
 }
---
+++ my-angular-quickstart/src/app/rgbColor.ts
@@ -0,0 +1,5 @@
+export class RGBcolor{
+    r:number;
+    g:number;
+    b:number;
+}
